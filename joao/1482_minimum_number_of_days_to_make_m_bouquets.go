f4fff265f966f9b1cc4bdd48bae1e8fb5fd1f71c
João Marcus Fernandes <53450523+joaomarcuslf@users.noreply.github.com>
2024-06-19 17:56:21 -0300

    Update and rename minimum_number_of_days_to_make_m_bouquets.go to 1482_minimum_number_of_days_to_make_m_bouquets.go

---
+++ joao/1482_minimum_number_of_days_to_make_m_bouquets.go
@@ -0,0 +1,37 @@
+func minDays(bloomDay []int, m int, k int) int {
+    n := len(bloomDay)
+    
+    if m * k > n {
+        return -1
+    }
+
+    l := slices.Min(bloomDay)
+	r := slices.Max(bloomDay)
+    
+	for l < r {
+		m := l + (r-l)/2
+
+        acc := 0
+		f := 0
+
+		for _, b := range bloomDay {
+			if b <= m {
+				f++
+				if f == k {
+					acc++
+					f = 0
+				}
+			} else {
+				f = 0
+			}
+		}
+
+		if acc >= m {
+			r = m
+		} else {
+			l = m + 1
+		}
+	}
+
+	return l
+}
